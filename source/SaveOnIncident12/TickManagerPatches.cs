002e6a17e81406f7c7eed0d51a9f2543259b498c
CGFighter <catgirlfighter@gmail.com>
2021-03-08 15:24:46 +0300

    1.0.3

--- source/SaveOnIncident12/TickManagerPatches.cs
+++ source/SaveOnIncident12/TickManagerPatches.cs
@@ -5,7 +5,7 @@ namespace SaveOnIncident
 {
     public static class TickManagerPatches
     {
-        [HarmonyPatch(typeof(TickManager), "Notify_GeneratedPotentiallyHostileMap")]
+        //[HarmonyPatch(typeof(TickManager), "Notify_GeneratedPotentiallyHostileMap")]
         static class StoryState_Notify_IncidentFired_SaveOnIncidentPatch
         {
             static void Postfix()
