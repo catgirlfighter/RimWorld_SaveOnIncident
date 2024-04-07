13882e5814c34f9072c0a911b9edf3fce1db8062
p.tychinin <catgirlfighter@gmail.com>
2024-04-07 17:44:59 +0300

    1.1

---
+++ source/SaveOnIncident15/Patches/TickManagerPatches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace SaveOnIncident.Patches
+{
+    public static class TickManagerPatches
+    {
+        [HarmonyPatch(typeof(TickManager), "Notify_GeneratedPotentiallyHostileMap")]
+        static class StoryState_Notify_IncidentFired_SaveOnIncidentPatch
+        {
+            internal static void Postfix()
+            {
+                if(Settings.generatedPotentiallyHostileMap) SaveOnIncident.SetupAutosave();
+            }
+        }
+    }
+}
