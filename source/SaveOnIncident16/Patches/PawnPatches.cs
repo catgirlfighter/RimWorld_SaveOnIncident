0b546283f10c1ca07244b500d4a9ca3c2b831dc4
p.tychinin <catgirlfighter@gmail.com>
2025-07-11 20:14:29 +0300

    1.6

---
+++ source/SaveOnIncident16/Patches/PawnPatches.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace SaveOnIncident.Patches
+{
+    [HarmonyPatch(typeof(Pawn), "Kill")]
+    static class Pawn_Kill_SaveOnIncidentPatch
+    {
+        internal static void Postfix(Pawn __instance)
+        {
+            if (__instance.Faction == Faction.OfPlayer)
+                if (__instance.RaceProps.Humanlike && Settings.saveAfterColonistDeath)
+                    SaveOnIncident.SetupAutosave();
+                else if (Settings.saveAfterColonyAnimalDeath)
+                    SaveOnIncident.SetupAutosave();
+        }
+    }
+}
